bf346f630970509fb0bf54091903e51574d5918e
sakshikanoongo <sakshikanoongo@9.81.213.205>
2020-01-08 12:33:59 +0530

    jar file added

---
+++ src/main/java/edureka/com/testproj_website/App.java
@@ -0,0 +1,13 @@
+package edureka.com.testproj_website;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/TestNG/headlessclassdemo.java
@@ -0,0 +1,37 @@
+package TestNG;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+
+public class headlessclassdemo {
+	
+	public static void main(String[] args) {
+		test();
+	}
+	public static void test() {
+		
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		
+		WebDriver driver = new ChromeDriver(options);
+		driver.get("http://35.194.210.186:9999/");
+		System.out.println(driver.getTitle());
+		driver.findElement(By.id("About Us")).click();
+		String x = driver.getPageSource();
+		System.out.println(x);
+		Assert.assertEquals(x, "<html><head></head><body><p id=\"PID-ab2-pg\">This is <b>about</b> page. Lorem Ipsum Dipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\n" + 
+				"<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>\n" + 
+				"</body></html>");
+		System.out.println("Website is deployed and is running fine");
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.close();
+		driver.quit();
+	}
+
+}
